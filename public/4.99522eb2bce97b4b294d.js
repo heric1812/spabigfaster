9fb542bd5a5bb512bcb983e5474f7c78dc4bb585
Herick Ruiz <heric1812@gmail.com>
2017-08-07 12:42:30 -0400

    commant build

---
+++ public/4.99522eb2bce97b4b294d.js
@@ -0,0 +1,1 @@
+webpackJsonp([4],{108:function(n,e,t){var r=t(8)(null,t(117),null,null,null);n.exports=r.exports},117:function(n,e){n.exports={render:function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},staticRenderFns:[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("section",[t("h1",[n._v("Sing Up")])])}]}}});
